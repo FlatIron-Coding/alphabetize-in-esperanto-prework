7187fd9c79ea814cd04dc4ca0eb386bd257c77f8
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-02 16:25:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,8 +2,6 @@ require 'pry'
 
 def alphabetize(arr)
   # code here
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
   
   sort_first_letter = arr.sort_by { |word| word[0] }
   sort_first_letter
