7f0eea86e35952ba5d3dad9141c2052daa37dc39
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-02 16:25:43 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,8 +5,4 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
   
-  sort_first_letter = arr.sort_by { |word| word[0] }
-  sort_first_letter
-  
-  binding.pry
 end
