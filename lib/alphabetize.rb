534ddcd92cfc37bd28d2d10c0e54f43a46657f8b
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-02 16:37:02 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
   arr.sort_by do |phrase|
-    phrase.split.map do |letter|
+    phrase.chars.map do |letter|
       ESPERANTO_ALPHABET.index(letter)
   
     end
